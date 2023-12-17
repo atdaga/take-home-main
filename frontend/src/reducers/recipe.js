f9b89b8c08a8b15af6369f14a26e9e5e2ea3c888
Anthony Daga <anthony@korrelated.com>
2023-12-17 00:26:49 -0800

    Added recipe actions/reducer.

--- frontend/src/reducers/recipe.js
+++ frontend/src/reducers/recipe.js
@@ -1,3 +1,35 @@
 /*
-  TODO: Create reducer and state updates here for recipe
+  TODO: Create reducer and state updates here for recipe. Done.
 */
+import { GET_RECIPE, RECEIVE_RECIPE, FAIL_GET_RECIPE } from "../actions"
+
+const initialState = {
+  recipe: null,
+  isLoading: false,
+  error: null,
+}
+
+const recipeFetching = (state) => {
+  return { ...state, recipe: null, isLoading: true, error: null }
+}
+
+const recipeFetched = (state, payload) => {
+  return { ...state, recipe: payload, isLoading: false }
+}
+
+const getRecipeFailed = (state, payload) => {
+  return { ...state, isLoading: false, error: payload }
+}
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+    case GET_SEARCH:
+      return recipeFetching()
+    case RECEIVE_SEARCH:
+      return recipeFetched(state, payload)
+    case FAIL_SEARCH:
+      return getRecipeFailed(state, payload)
+    default:
+      return state
+  }
+}
